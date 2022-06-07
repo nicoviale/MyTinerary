a328b68b098d2304389f4f0f6a0e86cc973fada4
nico <nico.viale769@gmail.com>
2022-06-07 18:53:15 -0300

    primer commit

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import NavBar from './components/AppBar.js';
+import Carrousel from './components/Carrousel.js';
+import "swiper/css/bundle";
+
+function App() {
+  return (
+    <div className="App">
+    <NavBar/>
+  
+    </div>
+  );
+}
+
+export default App;
